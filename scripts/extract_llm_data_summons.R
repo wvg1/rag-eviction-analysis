3dc6f018b545375df16e893602bac79e01625610
Will von Geldern <213677874+wvg1@users.noreply.github.com>
2026-02-15 14:00:58 -0800

    updated extraction script for summonses

--- scripts/extract_llm_data_summons.R
+++ scripts/extract_llm_data_summons.R
@@ -1,6 +1,6 @@
-###this script uses data_from_llm.py to document case characteristics from a variety of summons documents in eviction cases
-###data_from_llm.py uses chatgpt (with the prompt below) to answer questions about cases using summons documents
-###working directory should be the main folder of the eviction-data repo
+### this script uses data_from_llm.py to document case characteristics from a variety of summons documents in eviction cases
+### data_from_llm.py uses chatgpt (with the prompt below) to answer questions about cases using summons documents
+### working directory should be the main folder of the rag-eviction-analysis repo
 
 #load required packages
 library(tidyverse)
@@ -8,7 +8,7 @@ library(reticulate)
 library(jsonlite)
 
 #load virtual environment and python
-use_virtualenv("/Users/wvg1/Documents/eviction-data/.venv", required = TRUE)
+use_virtualenv("C:/dev/rag-eviction-analysis/.venv", required = TRUE)
 py_config()
 py <- import_builtins()
 pickle <- import("pickle")
